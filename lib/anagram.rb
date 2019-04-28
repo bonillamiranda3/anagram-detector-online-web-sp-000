2a749fdd86bddb5bcdff0df981113bcb40f5e323
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-04-28 21:53:22 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :words
+
+  def initialize(words)
+    @word = word
+  end
+  def match(word_array)
+    word_array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
+  ends
+end  
